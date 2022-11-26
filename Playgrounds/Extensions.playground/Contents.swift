e4489c92064e79f043bbef5d06088f621538313c
David Terwilliger <terwilld@gmail.com>
2022-11-25 23:03:06 -0500

    finish api delegate extension module

--- Playgrounds/Extensions.playground/Contents.swift
+++ Playgrounds/Extensions.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+extension Double {
+    func round(to places: Int){
+        print(places)
+        let precisionNumber = pow(10, Double(places))
+        var n = self
+        n = n * precisionNumber
+        n = n.rounded()
+        n = n / precisionNumber
+        print(n)
+        
+    }
+}
+
+extension UIButton {
+    func makeCircular() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.size.width / 2
+    }
+}
+
+
+var myDouble = 3.14159
+myDouble.round()
+print("fuck")
+
+
+myDouble.round(to: 3)
+
+var myButton =  UIButton(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+myButton.backgroundColor = .red
+myButton.makeCircular()
+
+
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+button.backgroundColor = .red
+
+button.layer.cornerRadius = 25
+button.clipsToBounds = true
+
+
